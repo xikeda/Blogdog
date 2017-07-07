77b0adf212f017130a82f2dee4f9bea352cebe6d
Ikeda <brandon_ikeda@hotmail.com>
2017-07-07 13:54:47 -0700

    Added authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  
   def index
     @posts = Post.all.order('created_at DESC')
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     @post.save
     if @post.save
