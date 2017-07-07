fc1f33542d8ce22723a2dd421e06fd980d63bee4
Ikeda <brandon_ikeda@hotmail.com>
2017-07-07 15:20:21 -0700

    Debugging

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
+
+  def new
+    @comment = current_user.comments.build
+  end
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment.comments.create(params[:comment].permit(:name, :body))
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
 
-    redirect_to post_path(@post)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
   end
 
   def destroy
@@ -15,4 +23,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  private
+
+        def comment_params
+            params.require(:comment).permit(:name, :body)
+        end
+
+        def find_comment
+            @post = Post.find(params[:post_id])
+        end
 end
